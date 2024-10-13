b000b3883118e618df0bb0ede211ff2db2d4d68b
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-13 23:00:42 +0600

    state counter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 import Users from './Users';
+import Counter from "./counter"
 
 function App() {
   let [data, setData] = useState("welcome to Cyberspace")
@@ -14,6 +15,10 @@ function App() {
     <div className="App">
       <h1>{data}</h1>
       <button onClick={updateData}>Update Value</button>
+      <br></br>
+      <Counter />
+
+
     </div>
   );
 }
